cb9b8b7d446744b37a6e502b56fb3afb89d61bbb
20171105123 <s20171105123@admindeimac-34.local>
2018-06-26 15:39:22 +0800

    Initial Commit

---
+++ 6.26the original code/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  6.26the original code
+//
+//  Created by 鲁祥 on 2018/6/26.
+//  Copyright © 2018年 鲁祥. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
